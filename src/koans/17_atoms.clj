1f582b19af7bdb063b4b0e6ca6a0a0cc75cb8799
Landon Morgan <morganlandon@gmail.com>
2020-07-23 15:25:53 -0500

    atoms practice complete

--- src/koans/17_atoms.clj
+++ src/koans/17_atoms.clj
@@ -5,29 +5,34 @@
 
 (meditations
   "Atoms are like refs"
-  (= __ @atomic-clock)
+  (= 0 @atomic-clock)
 
   "You can change at the swap meet"
-  (= __ (do
+  (= 1 (do
           (swap! atomic-clock inc)
           @atomic-clock))
 
+  ; https://www.youtube.com/watch?v=5ZL09WZIUb0
+  ; funny because the only incorrect buzzer in the
+  ; video is where I wanted help. Video is helpful
+  ; talk through of the atoms concept (special case of refs)
+
   "Keep taxes out of this: swapping requires no transaction"
   (= 5 (do
-         __
+         (swap! atomic-clock + 4)
          @atomic-clock))
 
   "Any number of arguments might happen during a swap"
-  (= __ (do
+  (= 20 (do
           (swap! atomic-clock + 1 2 3 4 5)
           @atomic-clock))
 
   "Atomic atoms are atomic"
-  (= __ (do
+  (= 20 (do
           (compare-and-set! atomic-clock 100 :fin)
           @atomic-clock))
 
   "When your expectations are aligned with reality, things proceed that way"
   (= :fin (do
-            (compare-and-set! __ __ __)
+            (compare-and-set! atomic-clock 20 :fin)
             @atomic-clock)))
